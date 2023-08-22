5121eb6c7c96ed8e2031f1ba64252fa74214bed7
BloodHound <81957270+BloodHound31@users.noreply.github.com>
2023-08-22 18:37:40 +0530

    Implementing MVVM architecture

---
+++ Login_Regitration_Clone/lib/Data/Network/NetworkApiService.dart
@@ -0,0 +1,17 @@
+import 'package:login_regitration_clone/Data/Network/BaseApiService.dart';
+
+class NetworkApiService extends BaseApiService{
+  @override
+  Future getGetApiResponse(String url) {
+    // TODO: implement getGetApiResponse
+    throw UnimplementedError();
+  }
+
+  @override
+  Future getPostApiResponse(String url) {
+    // TODO: implement getPostApiResponse
+    throw UnimplementedError();
+  }
+  
+  
+}
---
+++ Login_Regitration_Clone/lib/Data/app_exceptions.dart
@@ -0,0 +1,29 @@
+class AppException implements Exception{
+
+  final _prefix;
+  final _message;
+
+  AppException([this._prefix, this._message]);
+
+  String toString(){
+    return '$_prefix$_message';
+  }
+
+}
+
+
+class FetchDataException extends AppException{
+
+  FetchDataException([String? message]): super(message, 'Error During Fetching Data');
+}
+
+class BadRequestException extends AppException{
+
+  BadRequestException([String? message]): super(message, 'Invalid Request');
+}
+
+class UnauthorizedException extends AppException{
+
+  UnauthorizedException([String? message]): super(message, 'Unauthorized Request');
+}
+
